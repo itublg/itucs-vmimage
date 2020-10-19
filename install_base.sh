cd333a0f598f0eda91c06960da556b415c50d147
Mehmet Tahir SANDIKKAYA <sandikkaya@itu.edu.tr>
2020-10-19 20:10:42 +0300

    Added Xfce packages into base installation and switched to firefox-esr.

--- install_base.sh
+++ install_base.sh
@@ -3,7 +3,7 @@
 CHECK='dpkg-query --show'
 INSTALL='apt-get install -y --no-install-recommends'
 
-PACKAGES='vim patch git firefox owncloud-client'
+PACKAGES='xorg xfce4 xfce4-goodies vim patch git firefox-esr owncloud-client'
 
 for p in $PACKAGES; do
 	$CHECK $p || $INSTALL $p
