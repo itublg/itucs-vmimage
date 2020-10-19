6a121ca605d8cb37708fba6af46b405e3aa9b7c8
Mehmet Tahir SANDIKKAYA <sandikkaya@itu.edu.tr>
2020-10-19 20:04:44 +0300

    Required packages for BLG460E course have been presented as a script.
    The script is desgined to be used in conjunction wit a Vagrant file.

---
+++ install_blg460.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CHECK='dpkg-query --show'
+INSTALL='apt-get install -y --no-install-recommends'
+
+PACKAGES='terminator libcurl g++ gdb make splint default-mysql-client default-mysql-server mysql-workbench apache2 php-mysql php libapache2-mod-auth-mysql libapache2-mod-php php-mcrypt apache2-utils'
+
+for p in $PACKAGES; do
+	$CHECK $p || $INSTALL $p
+done
