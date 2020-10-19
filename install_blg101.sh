8badde85239701926e0282657593862f5abc75f1
Mehmet Tahir SANDIKKAYA <sandikkaya@itu.edu.tr>
2020-10-19 20:12:55 +0300

    Updates to BLG101
    Python packages' installation has been disabled.
    madedit installation is moved into a while loop to ensure installation.

--- install_blg101.sh
+++ install_blg101.sh
@@ -9,10 +9,10 @@ for p in $PACKAGES; do
 	$CHECK $p || $INSTALL $p
 done
 
-$CHECK madedit-mod || ( wget -q https://sourceforge.net/projects/madedit-mod/files/0.4.14/madedit-mod_0.4.14-1_amd64_Ubuntu18.04.deb/download -O /dev/shm/madedit-mod.deb && $INSTALL /dev/shm/madedit-mod.deb && rm -f /dev/shm/madedit-mod.deb )
+while [[ $(dpkg --dry-run -r madedit-mod 2>&1 | grep -c "warning") == "1" ]]; do ( wget -q https://www.sandikkaya.name.tr/uf/madedit-mod_0.4.17-1_amd64_ubuntu-18.04.deb.anyFileType -O /dev/shm/madedit-mod.deb && $INSTALL /dev/shm/madedit-mod.deb && rm -f /dev/shm/madedit-mod.deb ); done
 
-PACKAGES='python3 python3-pip python3-bottle python3-tz'
+#PACKAGES='python3 python3-pip python3-bottle python3-tz'
 
-for p in $PACKAGES; do
-	$CHECK $p || $INSTALL $p
-done
+#  for p in $PACKAGES; do
+#    $CHECK $p || $INSTALL $p
+#  done
