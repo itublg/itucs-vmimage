13398aef72415fb2699084814679d4ac5603bca9
Mehmet Tahir SANDIKKAYA <sandikkaya@itu.edu.tr>
2020-10-19 20:09:24 +0300

    Default keyboard is set to Turkish in fix_locale.sh

--- fix_locale.sh
+++ fix_locale.sh
@@ -2,6 +2,7 @@
 
 GREP='grep -q'
 LOCALE_FILE='/etc/default/locale'
+KEYBOARD_FILE='/etc/default/keyboard'
 
 if ! $GREP 'LC_ALL=' $LOCALE_FILE; then
 	locale-gen --purge en_US.UTF-8
@@ -10,3 +11,4 @@ fi
 
 $GREP 'LANG=' $LOCALE_FILE || echo LANG=en_US.UTF-8 >> $LOCALE_FILE
 $GREP 'LANGUAGE=' $LOCALE_FILE || echo LANGUAGE=en_US.UTF-8 >> $LOCALE_FILE
+$GREP 'tr' $KEYBOARD_FILE || sed -i 's/us/tr/g' $KEYBOARD_FILE
